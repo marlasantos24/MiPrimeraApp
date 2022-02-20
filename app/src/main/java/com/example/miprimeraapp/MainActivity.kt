5f27dd794d77c61fb28916c8c2ff3fbd63c3b5f8
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-02-20 12:29:35 -0500

    Uniendo_VistaConCodigo

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import  android.widget.Button
+import  android.widget.CheckBox
+import  android.widget.EditText
+import  android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun validarDato(): Boolean{
+        val etNombre= findViewById<EditText>(R.id.etNombre)
+        val nombreUsuario = etNombre.text
+
+        if(nombreUsuario.isNullOrEmpty()){
+            return false
+        }
+
+        return true
     }
 }
 //Prueba de Commit
